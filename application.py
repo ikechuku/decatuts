86620bb27e4ec26b19139e71892429d7c6ce7d5b
ikechuku <rockstarik2@gmail.com>
2019-09-02 15:53:24 +0100

    (backend):Created registration route

---
+++ application.py
@@ -0,0 +1,177 @@
+import os
+import datetime
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, usd
+
+# Configure application
+app = Flask(__name__)
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///decatuts.db")
+
+# # Make sure API key is set
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Render Index Page"""
+    # print("all_stock", all_stock)
+
+    
+
+    return render_template("index.html")
+
+
+
+
+@app.route("/check", methods=["GET"])
+def check():
+    """Return true if email available, else false, in JSON format"""
+
+    email =  request.args.get("email")
+    print(jsonify(False))
+
+    print(email)
+    rows = db.execute("SELECT 1 FROM users WHERE email = :email", email=email)
+    print(rows)
+    if len(rows) >0:
+        return jsonify(False)
+
+    return jsonify(True)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide email", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for email
+        rows = db.execute("SELECT * FROM users WHERE email = :email",
+                          email=request.form.get("email"))
+
+        # Ensure email exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid email and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        session['email'] = rows[0]['email']
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        # get all input values and store in python variables
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        phone = request.form.get("phone")
+        reference = request.form.get("selected")
+    
+        # print("Selected", selected)
+
+        # print(email, password, confirmation)
+        # User reached route via POST (as by submitting a form via POST)
+        if not email or email == "":
+            return apology("must provide email", 400)
+
+        rows = db.execute("SELECT * FROM users WHERE email = :email", email=email)
+        print("^^^^^the rows",len(rows))
+        
+        if len(rows)>0:
+            print("Passwords not provided")
+            return apology("User already exists", 400)
+         # Ensure password was submitted
+        elif not password :
+            print("Passwords not provided")
+
+            return apology("must provide password", 400)
+
+        if password!=confirmation:  
+            print("Passwords do not match", 400)
+            return apology("Passwords do not match", 400)
+        
+        hashed_pwd = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+        # Insert into the  database
+        db.execute("INSERT INTO users (firstname, lastname, email, phone, reference, hash)  VALUES (:firstname, :lastname, :email, :phone, :reference, :hash)", firstname=firstname, lastname=lastname,  email=email, phone=phone, reference=reference, hash=hashed_pwd)
+        return redirect("/login")      
+
+    else:
+        return render_template("register.html")
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
