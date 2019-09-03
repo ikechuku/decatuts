a8d7d2bd7e9064f040f2cdf6b264103e72db260e
Femi <phemmy09@gmail.com>
2019-09-03 11:55:42 +0100

    Created student route

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, usd
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
@@ -25,9 +25,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
@@ -37,20 +34,54 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///decatuts.db")
 
-# # Make sure API key is set
-# if not os.environ.get("API_KEY"):
-#     raise RuntimeError("API_KEY not set")
-
 
 @app.route("/")
 @login_required
 def index():
     """Render Index Page"""
-    # print("all_stock", all_stock)
-
-    
-
     return render_template("index.html")
+
+
+@app.route("/students", methods=["GET", "POST"])
+@login_required
+def students():
+    # User reached route via GET
+    if request.method == "GET":
+        return render_template("students.html")
+
+    # User reached route via POST (as by submitting a form via POST)
+    elif request.method == "POST":
+        subject = request.form.get("subject")
+        tutor_gender = request.form.get("tutor_gender")
+        address = request.form.get("address")
+
+        print(subject, tutor_gender, address)
+        row = db.execute("INSERT INTO students (user_id, subject, tutor_gender, address) VALUES (:user_id, :subject, :tutor_gender, :address)", user_id=session['user_id'], subject=subject,tutor_gender=tutor_gender,address=address)
+
+        print(row)
+        return redirect("/schedule")  
+
+
+
+@app.route("/schedule", methods=["GET", "POST"])
+@login_required
+def schedule():
+    # User reached route via GET
+    if request.method == "GET":
+        return render_template("schedule.html")
+
+    # User reached route via POST (as by submitting a form via POST)
+    elif request.method == "POST":
+        email = session
+        subject = request.form.get("subject")
+        tutor_gender = request.form.get("tutor_gender")
+        address = request.form.get("address")
+
+        print(subject, tutor_gender, address)
+        row = db.execute("INSERT INTO students (user_id, subject, tutor_gender, address) VALUES (:user_id, :subject, :tutor_gender, :address)", user_id=session['user_id'], subject=subject,tutor_gender=tutor_gender,address=address)
+
+        print(row)
+        return redirect("/")  
 
 
 
@@ -60,12 +91,8 @@ def check():
     """Return true if email available, else false, in JSON format"""
 
     email =  request.args.get("email")
-    print(jsonify(False))
-
-    print(email)
     rows = db.execute("SELECT 1 FROM users WHERE email = :email", email=email)
-    print(rows)
-    if len(rows) >0:
+    if len(rows) > 0:
         return jsonify(False)
 
     return jsonify(True)
@@ -74,7 +101,6 @@ def check():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-    print("here we are")
     # Forget any user_id
     session.clear()
 
@@ -133,10 +159,6 @@ def register():
         phone = request.form.get("phone")
         reference = request.form.get("selected")
     
-        # print("Selected", selected)
-
-        # print(email, password, confirmation)
-        # User reached route via POST (as by submitting a form via POST)
         if not email or email == "":
             return apology("must provide email", 400)
 
